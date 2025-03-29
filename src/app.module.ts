7d5f19c9b71c58bfac3daff57a414f548e290702
Daniel Pereira <daniel.n10@hotmail.com>
2025-03-28 23:01:13 -0300

    feat: include answers and resources module into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { dataSourceOptions } from 'src/data-source';
+import { ResourcesModule } from '@/resources/resources.module';
+import { AnswersModule } from '@/answers/answers.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UsersModule],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    UsersModule,
+    ResourcesModule,
+    AnswersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
